555fff50008b20c622137039273dad4268c0faaf
山泉农夫 <1942480291@qq.com>
2019-01-04 02:35:27 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,5 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 //个人页面
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

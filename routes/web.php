5b60e2c0752c9b7d429395ed6b13c2685d864c22
山泉农夫 <1942480291@qq.com>
2019-01-08 01:38:42 +0000

    翻译URL标示

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,8 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //个人页面
 Route::resource('users','UsersController',['only'=>['show','edit','update']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}','TopicsController@show')->name('topics.show');
 
 //分类下的话题列表
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);

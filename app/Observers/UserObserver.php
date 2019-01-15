c097142a6da85dd8bfddeb601992af33bf126ef7
山泉农夫 <1942480291@qq.com>
2019-01-15 05:56:31 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,13 @@ class UserObserver
     {
         //
     }
+
+    //在用户数据即将入库之前，将为 avatar 字段设置一张默认的头像
+    public function saving(User $user)
+    {
+    	// 这样写扩展性更高，只有空的时候才指定默认头像
+    	if(empty($user->avatar)){
+    		$user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+    	}
+    }
 }
